4a271842a110c1ea930dbb0d802185869b1e5d96
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2024-02-11 14:02:54 -0800

    Initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contacts';
+
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
+export const persistor = store;
